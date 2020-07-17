33c44387f988d75fd12b2423cbf461021fe09d67
Philipp Schmitt <philipp@schmitt.co>
2020-07-17 12:59:36 +0200

    Allow outlets to be identified by name

--- cli.py
+++ cli.py
@@ -3,9 +3,14 @@
 
 import argparse
 import json
+import logging
 import os
+import re
 import time
 from api import IPU
+
+
+LOGGER = logging.getLogger("pdu.cli")
 
 
 def parse_args():
@@ -25,26 +30,62 @@ def parse_args():
     return parser.parse_args()
 
 
+def get_outlet_states(pdu):
+    states = pdu.status().get("outlet_states")
+    outlets = {int(_id[-1]): name for (_id, name) in pdu.outlet_names()}
+    output = {}
+    for o_id, outlet in outlets.items():
+        output[outlet] = {"id": o_id, "state": states[o_id]}
+    return output
+
+
+def get_outlet_id(pdu, name):
+    states = get_outlet_states(pdu)
+    for outlet_name, outlet_data in states.items():
+        if name == outlet_name:
+            return outlet_data.get("id")
+
+
+def get_outlet_ids(pdu, outlets):
+    if not isinstance(outlets, list):
+        outlets = [outlets]
+    outlet_ids = []
+    states = get_outlet_states(pdu)
+    for o in outlets:
+        if isinstance(o, int) or re.match(r"\d+", o):
+            outlet_ids.append(o)
+        else:
+            for name, data in states.items():
+                if name == o:
+                    outlet_ids.append(data.get("id"))
+    return outlet_ids
+
+
 def main():
     args = parse_args()
     if args.debug:
-        print(args)
+        logging.basicConfig(level=logging.DEBUG)
+        LOGGER.debug(f"ARGS: {args}")
     pdu = IPU(args.hostname, auth=(args.username, args.password))
+    outlet_ids = get_outlet_ids(pdu, args.OUTLETS) if args.OUTLETS else None
+    if args.debug:
+        LOGGER.debug(f"Outlet IDS: {outlet_ids}")
     if args.ACTION in ["toggle", "t"]:
-        pdu.disable_outlets(args.OUTLETS)
+        LOGGER.debug(f"Toggling outlets {outlet_ids}")
+        pdu.disable_outlets(outlet_ids)
         time.sleep(1)
-        pdu.enable_outlets(args.OUTLETS)
+        pdu.enable_outlets(outlet_ids)
     elif args.ACTION in ["on", "o", "enable", "e"]:
-        pdu.enable_outlets(args.OUTLETS)
+        LOGGER.debug(f"Enabling outlets {outlet_ids}")
+        pdu.enable_outlets(outlet_ids)
     elif args.ACTION in ["off", "f", "disable", "d"]:
-        pdu.disable_outlets(args.OUTLETS)
+        LOGGER.debug(f"Disabling outlets {outlet_ids}")
+        pdu.disable_outlets(outlet_ids)
     elif args.ACTION in ["states"]:
-        states = pdu.status().get("outlet_states")
-        outlets = {int(_id[-1]): name for (_id, name) in pdu.outlet_names()}
-        for o_id, outlet in outlets.items():
-            print(f"{outlet}: {states[o_id]}")
+        output = get_outlet_states(pdu)
+        print(json.dumps(output))
 
-    elif args.ACTION in ["state", "status", "st", "s"]:
+    elif args.ACTION in ["status", "st", "s"]:
         print(json.dumps(pdu.status()))
 
 
